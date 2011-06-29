fbeab56bc6e55e4b19f2baa3049b28473f7fb936
nin-jin <nin-jin@ya.ru>
2011-06-29 08:51:02 +0400

    export

---
+++ export.php
@@ -0,0 +1,67 @@
+<?php
+
+function rrmdir($dir) { 
+   if (is_dir($dir)) { 
+     $objects = scandir($dir); 
+     foreach ($objects as $object) { 
+       if ($object != "." && $object != "..") { 
+         if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object); 
+       } 
+     } 
+     reset($objects); 
+     rmdir($dir); 
+   } 
+ }
+
+function rchdir($dir) {
+   chdir( $dir ) or die( "Can not chdir to [{$dir}]");
+}
+
+system( 'git checkout-index -a -f --prefix=-export/' );
+rchdir( '-export' );
+	unlink( '.gitmodules' );
+	unlink( 'export.php' );
+    unlink( 'index.php' );
+    foreach( glob( '*', GLOB_ONLYDIR ) as $pack ):
+		if( $pack[0] === '-' ) continue;
+        rchdir( $pack );
+            rchdir( '-mix' );
+                copy( 'compiled.css', 'index.css' );
+                copy( 'compiled.xsl', 'index.xsl' );
+                copy( 'compiled.js', 'index.js' );
+            rchdir( '..' );
+            rchdir( '-mix+doc' );
+                copy( 'compiled.css', 'index.css' );
+                copy( 'compiled.xsl', 'index.xsl' );
+                copy( 'compiled.js', 'index.js' );
+            rchdir( '..' );
+            foreach( glob( '*', GLOB_ONLYDIR ) as $module ):
+			    if( $module[0] === '-' ) continue;
+                rchdir( $module );
+                    foreach( glob( '*.tree' ) as $file ):
+                        unlink( $file );
+                    endforeach;
+                    foreach( glob( '*.css' ) as $file ):
+                        unlink( $file );
+                    endforeach;
+                    foreach( glob( '*.jam' ) as $file ):
+                        unlink( $file );
+                    endforeach;
+                    foreach( glob( '*.js' ) as $file ):
+                        unlink( $file );
+                    endforeach;
+                    foreach( glob( '*.vml' ) as $file ):
+                        unlink( $file );
+                    endforeach;
+                    foreach( glob( '*.xsl' ) as $file ):
+                        unlink( $file );
+                    endforeach;
+                rchdir( '..' );
+                @rmdir( $module );
+            endforeach;
+        rchdir( '..' );
+        @rmdir( $pack );
+    endforeach;
+rchdir( '..' );
+
+header( 'Location: -export/', true, 302 );
