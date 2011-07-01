8760163bfca60bef880d91dfa1f5d96c2eb1625d
nin-jin <nin-jin@ya.ru>
2011-07-01 11:13:37 +0400

    wc-css3 wc-button

--- jam/Throttler/jam+Throttler.js
+++ jam/Throttler/jam+Throttler.js
@@ -2,10 +2,17 @@ with( $jam )
 $define
 (	'$Throttler'
 ,	function( latency, func ){
+		var self
+		var arg
 		var stop
 		return function(){
-			if( stop ) stop= stop()
-			stop= $schedule( latency, func )
+			self= this
+			arg= arguments
+			if( stop ) return
+			stop= $schedule( latency, function(){
+				stop= null
+				func.apply( self, arg )
+			})
 		}
 	}
 )
