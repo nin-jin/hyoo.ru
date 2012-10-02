03bf7b730f4617f4d28469f22370926397ffb0c8
nin-jin <nin-jin@ya.ru>
2012-10-02 05:03:07 +0400

    web-components -> PMS

--- git-init.cmd
+++ git-init.cmd
@@ -1,1 +1,1 @@
-git init && git config remote.origin.url git://github.com/nin-jin/web-components.git && git config remote.origin.pushurl git@github.com:nin-jin/web-components.git && git pull
+git init && git config remote.origin.url git://github.com/nin-jin/PMS.git && git config remote.origin.pushurl git@github.com:nin-jin/PMS.git && git pull
