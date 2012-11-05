0f319d8baf09512aca47306c8c5f683e2b78832a
nin-jin <nin-jin@ya.ru>
2012-11-05 18:28:00 +0400

    фикс недобавления локальных файлов в гит при инициализации

--- git-init.cmd
+++ git-init.cmd
@@ -1,1 +1,1 @@
-git init && git config remote.origin.url git://github.com/nin-jin/PMS.git && git config remote.origin.pushurl git@github.com:nin-jin/PMS.git && git pull
+git init && git config remote.origin.url git://github.com/nin-jin/PMS.git && git config remote.origin.pushurl git@github.com:nin-jin/PMS.git && git pull & git add .
